b34242e5ad8d8d805e7c5ada80f8a66e9864a435
BsoH <mike.orsoli@hotmail.com>
2017-05-13 13:14:50 +0000

    Added Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   root 'pages#index'
   # define routes for Pages
 
-  get 'home' => 'pages#home' #override default rotes
-  get 'profile' => 'pages#profile'
-  get 'explore' => 'pages#explore'
+  get '/home' => 'pages#home' #override default rotes
+  get '/profile' => 'pages#profile'
+  get '/explore' => 'pages#explore'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
